9139b651e838d33992dacc4fdd28991115420336
Vishal D <vishal.msus@gmail.com>
2026-08-18 13:44:45 -0400

    chore: normalize terraform file encoding

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,4 +1,4 @@
-﻿terraform {
+terraform {
   backend "s3" {
     bucket         = "terraform-state-145405817961-us-east-1"
     key            = "taskmanager/terraform.tfstate"
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-﻿terraform {
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
