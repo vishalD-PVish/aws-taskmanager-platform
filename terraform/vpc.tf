2ae1a9b3769a43e466a50c3b36cdd6d8c52fa59b
vishalD-PVish <vishal.msus@gmail.com>
2026-08-18 14:07:46 -0400

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -77,7 +77,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0](#cite-0).id
+  subnet_id     = aws_subnet.public[0]
 
   tags = {
     Name = "taskmanager-nat"
