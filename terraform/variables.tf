21fac52d5afe6ae6c37d7b965507fc620d8e11fc
Vishal D <vishal.msus@gmail.com>
2026-09-03 18:25:32 -0400

    feat: package alembic migrations in ecs image

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -62,6 +62,7 @@ variable "api_desired_count" {
 variable "container_image_tag" {
   description = "The ECR image version our task will reference"
   type        = string
-  default     = "0.3.0"
+  default     = "0.4.0"
 }
+
 
